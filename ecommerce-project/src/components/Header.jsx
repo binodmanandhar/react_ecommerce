58e5f45cc09fe88da3203459df5fad0cfac87b7a
Binod Manandhar <binodmanandhar7@gmail.com>
2026-02-16 13:51:10 +1100

    Add ecommerce backend

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -6,7 +6,7 @@ export function Header() {
     <>
       <div className="header">
         <div className="left-section">
-          <Link to="/" className="header-link test">
+          <Link to="/" className="header-link">
             <img className="logo" src="images/logo-white.png" />
             <img className="mobile-logo" src="images/mobile-logo-white.png" />
           </Link>
